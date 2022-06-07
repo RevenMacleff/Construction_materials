bea34f177dc15c09929317108128ba92d8f41768
Rasul <rasulrizvan3@gmail.com>
2022-06-07 20:20:45 +0600

    timer

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,17 @@
 import "./slider";
 import tabs from './modules/tabs';
 import modals from "./modules/modal"; 
+import timer from './modules/timer';
 window.addEventListener("DOMContentLoaded", () =>{
+    "use strict";
+
+    let deadline = '2020-02-01';
+
+    
  modals();
  tabs('.glazing_slider','.glazing_block','.glazing_content','active');
  tabs('.decoration_slider',".no_click",".decoration_content > div > div",'after_click');
+ timer('.container1', deadline);
 });
 
 
